2f36d85a869aae5f34c94f0acfe5b793204c7226
AmilDean <79336011+AmilDean@users.noreply.github.com>
2024-07-11 13:32:28 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,7 @@ if (document.body.classList.contains('index-page')) {
 		"Attack on Titan",
 		"Tokyo Ghoul",
 		"No Game No Life",
-		"Parasyte the maxim",
+		"Parasyte the Maxim",
 		"Hunter x Hunter",
 		"Swords Art Online",
 		"Full Metal Alchemist Brotherhood",
@@ -797,9 +797,15 @@ animeList.forEach(anime => {
     animeCard.classList.add('anime-card');
 
     const animeImage = document.createElement('img');
-    const formattedAnime = anime.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('-');
-    const lowerCaseAnime = anime.replace(/\s+/g, '-').toLowerCase();
-    const mixedCaseAnime = anime.split(' ').map((word, index) => index % 2 === 0 ? word.charAt(0).toUpperCase() + word.slice(1).toLowerCase() : word.toLowerCase()).join('-');
+
+    // Function to format anime titles
+    const formatTitle = (title, separator = '-') => {
+        return title.replace(/[^\w\s-]/g, '').split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(separator);
+    };
+
+    const formattedAnime = formatTitle(anime);
+    const lowerCaseAnime = formatTitle(anime).toLowerCase();
+    const mixedCaseAnime = anime.replace(/[^\w\s-]/g, '').split(' ').map((word, index) => index % 2 === 0 ? word.charAt(0).toUpperCase() + word.slice(1).toLowerCase() : word.toLowerCase()).join('-');
 
     animeImage.src = `images/${formattedAnime}.jpg`;
     animeImage.alt = anime;
@@ -825,9 +831,5 @@ animeList.forEach(anime => {
     animeCard.appendChild(animeTitle);
     container.appendChild(animeCard);
 });
-
-
-
-
 
 }
